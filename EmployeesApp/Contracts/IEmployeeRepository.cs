86e6255a9277ac3fee31e694b7a64fbb125c5753
Olga Shtykova <olgashtykova@yahoo.com>
2021-06-16 21:32:17 +0300

    Add project files.

---
+++ EmployeesApp/Contracts/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeesApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace EmployeesApp.Contracts
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetEmployee(Guid id);
+        void CreateEmployee(Employee employee);
+    }
+}
